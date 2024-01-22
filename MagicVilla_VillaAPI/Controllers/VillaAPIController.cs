cd220393218d10569b4d62bf88b98aeabfc5fe41
pavan <mani365@gmail.com>
2024-01-22 12:35:43 +0530

    committing.

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MagicVilla_VillaAPI.Models;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [ApiController]
+   [Route("api/[controller]")]
+    public class VillaAPIController:ControllerBase
+
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa{Id=1,Name="Magroove Resort"},
+                new Villa{Id=2,Name="ECR Resort and Villas"}
+            };
+        }
+    }
+}
